a83e12b601a57242a180764c0becf9a9d6d96176
PabloPiliavsky <pablopiliavsky21@gmail.com>
2023-05-29 22:52:29 -0300

    cambio en el root para poder usar la db

--- templates_c/c9ec8ee8df873907383a76f5fedeb154cb40db39_0.file.jugador.tpl.php
+++ templates_c/c9ec8ee8df873907383a76f5fedeb154cb40db39_0.file.jugador.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.0, created on 2023-05-30 03:42:42
+/* Smarty version 4.3.0, created on 2023-05-30 03:49:47
   from 'C:\xampp\htdocs\TPE_WEB2\templates\jugador.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.0',
-  'unifunc' => 'content_647554921f26f8_82615094',
+  'unifunc' => 'content_6475563b6f08d8_37949891',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'c9ec8ee8df873907383a76f5fedeb154cb40db39' => 
     array (
       0 => 'C:\\xampp\\htdocs\\TPE_WEB2\\templates\\jugador.tpl',
-      1 => 1685395673,
+      1 => 1685411250,
       2 => 'file',
     ),
   ),
@@ -22,26 +22,28 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:footer.tpl' => 1,
   ),
 ),false)) {
-function content_647554921f26f8_82615094 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6475563b6f08d8_37949891 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender('file:header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
+
     <section>
-        
-       
-       <p>  <?php echo $_smarty_tpl->tpl_vars['jugador']->value->nombre;?>
+       <h1><?php echo $_smarty_tpl->tpl_vars['jugador']->value->nombre;?>
+</h1>
+       <h2><?php echo $_smarty_tpl->tpl_vars['jugador']->value->apellido;?>
+</h2>
+       <p><?php echo $_smarty_tpl->tpl_vars['jugador']->value->descripcion;?>
 </p>
-       <p>  <?php echo $_smarty_tpl->tpl_vars['jugador']->value->apellido;?>
+       <h3><?php echo $_smarty_tpl->tpl_vars['jugador']->value->posicion;?>
+</h3>
+       <p><?php echo $_smarty_tpl->tpl_vars['jugador']->value->id_pais;?>
 </p>
-       <p>  <?php echo $_smarty_tpl->tpl_vars['jugador']->value->descripcion;?>
-</p>
-       <p>  <?php echo $_smarty_tpl->tpl_vars['jugador']->value->posicion;?>
-</p>
-       <p>  <?php echo $_smarty_tpl->tpl_vars['jugador']->value->id_pais;?>
-</p>
-       <img src=" <?php echo $_smarty_tpl->tpl_vars['jugador']->value->foto;?>
- ">
-        
+       <img src="<?php echo $_smarty_tpl->tpl_vars['jugador']->value->foto;?>
+">
     </section>
+
+   
+
+
 
 <?php $_smarty_tpl->_subTemplateRender('file:footer.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 }
